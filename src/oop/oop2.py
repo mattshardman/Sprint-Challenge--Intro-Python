cd894b700243159885423521389c1873e2c3fbd8
mattshardman <mattshardman@gmail.com>
2019-10-04 08:24:51 +0100

    task: complete oop2.py all tests passing

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,10 +4,12 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
     # TODO
+    def drive(self):
+        return "vroooom"
 
 
 # Subclass Motorcycle from GroundVehicle.
@@ -19,6 +21,15 @@ class GroundVehicle():
 
 # TODO
 
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super()
+        self.num_wheels = 2
+
+    def drive(self):
+        return "BRAAAP!!"
+
+
 vehicles = [
     GroundVehicle(),
     GroundVehicle(),
@@ -27,6 +38,10 @@ vehicles = [
     Motorcycle(),
 ]
 
+
+
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+for v in vehicles:
+    print(v.drive())
